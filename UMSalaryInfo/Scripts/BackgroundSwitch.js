88ba8b37d50fb89db2332c844b9ab018867dcc49
shray7 <shray.7@gmail.com>
2016-04-21 00:26:58 -0400

    Background change

---
+++ UMSalaryInfo/Scripts/BackgroundSwitch.js
@@ -0,0 +1,25 @@
+﻿var backgroundImage = 1;
+function changeStyle() {
+
+
+    if (backgroundImage == 1) {
+        $('body').css({ backgroundImage: 'url(content/lawschool.jpg)' });
+        backgroundImage = 2;
+    }
+
+    else if (backgroundImage == 2) {
+        $('body').css({ backgroundImage: 'url(content/hogwarts.jpg)' });
+        backgroundImage = 3;
+    }
+
+    else if (backgroundImage == 3) {
+        $('body').css({ backgroundImage: 'url(content/bschool.jpg)' });
+        backgroundImage = 4;
+    }
+
+    else if (backgroundImage == 4) {
+        $('body').css({ backgroundImage: 'url(content/mstadium.jpg)' });
+        backgroundImage = 1;
+    }
+
+}
